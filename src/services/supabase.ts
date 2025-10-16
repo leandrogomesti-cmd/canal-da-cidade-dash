f8e600c26560cd780c8a0c13c1a2e35dfd0791d8
Leandro Gomes <leandrogomesti@gmail.com>
2025-10-16 06:19:41 -0300

    alteracao apontamento supabase

--- src/services/supabase.ts
+++ src/services/supabase.ts
@@ -1,7 +1,7 @@
 // src/lib/supabase.ts
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://yckouwzjejhqwpmbdbkj.supabase.co'
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inlja291d3pqZWpocXdwbWJkYmtqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzU2MDk3NDYsImV4cCI6MjA1MTE4NTc0Nn0._n4XU02deVJmEzwXfuBPuMI3uAUaRctpxfGgxqsNRGA'
+const supabaseUrl = 'https://dolgzyqsdphlabnpwdku.supabase.co'
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRvbGd6eXFzZHBobGFibnB3ZGt1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjAxMzczMjAsImV4cCI6MjA3NTcxMzMyMH0.rW7kmsWef-2Cw1orUdrmFBcAd4Bu9bgJTx77Al1_VME'
 
 export const supabase = createClient(supabaseUrl, supabaseKey)
